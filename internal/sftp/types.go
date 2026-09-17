d3d8b47e4cf437fde4adf5bdc515e821bc94f5e0
Masaki Aida <51500566+aida0710@users.noreply.github.com>
2026-09-17 15:31:26 +0900

    style(sftp): 転送操作の定義をgofmtで整える

--- internal/sftp/types.go
+++ internal/sftp/types.go
@@ -134,8 +134,8 @@ const (
 	RemoteCopy   RemoteTransferOperation = "copy"
 	RemoteMove   RemoteTransferOperation = "move"
 	RemoteDelete RemoteTransferOperation = "delete"
-	RemoteGet RemoteTransferOperation = "get"
-	RemotePut RemoteTransferOperation = "put"
+	RemoteGet    RemoteTransferOperation = "get"
+	RemotePut    RemoteTransferOperation = "put"
 )
 
 type RemoteTransferRequest struct {
